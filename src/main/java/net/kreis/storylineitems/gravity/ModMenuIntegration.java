1e0da5c27e261d90c5ddd92905dd232977ec0781
Daus <99434750+Kooreis@users.noreply.github.com>
2025-01-16 13:33:02 -0600

    added light stone and dark stone, as well as functional item groups. fixed render issue

---
+++ src/main/java/net/kreis/storylineitems/gravity/ModMenuIntegration.java
@@ -0,0 +1,13 @@
+package net.kreis.storylineitems.gravity;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import gravity_changer.config.GravityChangerConfig;
+import me.shedaniel.autoconfig.AutoConfig;
+
+public class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> AutoConfig.getConfigScreen(GravityChangerConfig.class, parent).get();
+    }
+}
---
+++ src/main/java/net/kreis/storylineitems/mixin/PlayerListMixin.java
@@ -0,0 +1,8 @@
+package net.kreis.storylineitems.mixin;
+
+import net.minecraft.server.players.PlayerList;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(PlayerList.class)
+public abstract class PlayerListMixin {
+}
